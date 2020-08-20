442a13b5254b4cd5871977404eb76ebc816ada9b
Javier Erazo <alexerazoajebsk8@outlook.com>
2020-08-20 01:03:02 -0500

    Primer Commit

---
+++ src/Control/NewMain.java
@@ -0,0 +1,16 @@
+package Control;
+
+
+import View.Login;
+import View.ModContrib.ModContribuyentes;
+import View.Modulos;
+
+public class NewMain {
+    public static void main(String[] args) {
+        ModContribuyentes in = new ModContribuyentes();
+        in.setVisible(true);
+        
+         
+    }
+    
+}
