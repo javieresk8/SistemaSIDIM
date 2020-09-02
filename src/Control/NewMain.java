ad60950973d86c414dcf016419f6c43d63f3f3e4
SalinVargas <vargas.andres722@gmail.com>
2020-09-02 12:23:02 -0500

    avance 2

--- src/Control/NewMain.java
+++ src/Control/NewMain.java
@@ -3,11 +3,12 @@ package Control;
 
 import View.Login;
 import View.ModContrib.ModContribuyentes;
+import View.ModReportes.Mod_Reportes;
 import View.Modulos;
 
 public class NewMain {
     public static void main(String[] args) {
-        ModContribuyentes in = new ModContribuyentes();
+        Mod_Reportes in = new Mod_Reportes();
         in.setVisible(true);
         
          
